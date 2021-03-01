8a4aa45f88d331343a53896d9d7b89db9ee330f7
Zeekerz <pramote.p.on@gmail.com>
2021-03-01 22:01:39 +0700

    Functional Programming in JavaScript

--- index.js
+++ index.js
@@ -245,3 +245,41 @@ console.log('Add Five: ', addFive(10));
 console.log('Add Ten: ', addTen(10));
 
 // Recursion
+function countDown(n){
+    if(n < 0) return;
+    console.log('Count down', n);
+    countDown(n-1);
+}
+countDown(10);
+// Base case 
+// f(1) = 1;
+// Recursive case 
+// f(2) = 2 * 1;
+// f(3) = 3 * 2 * 1;
+// f(4) = 4 * 3 * 2 * 1;
+// or
+// f(1) = 1;
+// f(2) = 2 * f(1);
+// f(3) = 3 * f(2);
+// f(4) = 4 * f(3);
+
+function factorial(n){
+    if(n === 1) return 1;
+    return n * factorial(n-1);
+}
+console.log('Factorial: ', factorial(4));
+
+// c => c
+// bc => cb
+// abc => cba
+// or
+// c => c
+// bc => reverse(c) + b
+// abc => reverse(bc) + a
+function reverse(str){
+    if(str.length === 1) return str;
+    const [firstCharacter] = str;
+    const remainingCharacters = str.substring(1);
+    return reverse(remainingCharacters) + firstCharacter;
+}
+console.log('Reverse: ', reverse('abc'));
